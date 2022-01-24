1b52e3a371a720ac487f841d53e65b42dfdc09a1
Sumeet Vishwakarma <sumeetgithub@gmail.com>
2022-01-24 18:50:53 +0530

    DONE

---
+++ inputandoutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "enter the number" << endl;
+    cin >> number;
+    cout << "number : "<< number << endl;
+}
+
+
---
+++ mutipleINPUTANDOUTPUT.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int number;
+    string address;
+    cout << "enter the name" << endl;
+    cin >> name;
+    cout << "enter the number" << endl;
+    cin >> number;
+    cout << "enter the address" << endl;
+    cin >> address;
+    cout << "Name : " << name << endl;
+    cout << "number : " << number << endl;
+    cout << "address : " << address << endl;
+    return 0;
+}
