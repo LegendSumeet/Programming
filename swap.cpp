00645953ea52f08ac566b7e3305efd4700045581
Sumeet Vishwakarma <sumeetgithub@gmail.com>
2022-01-27 15:11:26 +0530

    dffd

--- swap.cpp
+++ swap.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int a, b, temp;
     cout << "Enter the Numbers" << endl;
     cin >> a >> b;
-
     cout << "The numbers" << endl;
     cout << "a = " << a << " b = " << b << endl;
-
     temp = a;
     a = b;
     b = temp;
-
     cout << "After swapping the given numbers." << endl;
     cout << "a = " << a << " b = " << b << endl;
-
     return 0;
 }
