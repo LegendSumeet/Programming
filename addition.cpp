1eee74ca83d20c79047b646190ae31ba47a63def
Sumeet Vishwakarma <sumeetgithub@gmail.com>
2022-01-27 14:38:14 +0530

    kfkf

--- addition.cpp
+++ addition.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+    int firstNumber,
+        secondNumber, sumOfTwoNumbers;
+
     cout << "Enter two integers: ";
+
     cin >> firstNumber >> secondNumber;
+
     sumOfTwoNumbers = firstNumber + secondNumber;
+
     cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers << endl;
+
     return 0;
 }
