bd6b259dba3f54cde4d7fe79e77d496f6e3f051c
Sumeet Vishwakarma <sumeetgithub@gmail.com>
2022-01-28 14:28:42 +0530

    done with

--- acciiiiii.cpp
+++ acciiiiii.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 long long Ascii(string sumeet)
 {
-	long long sumit = 1;
-
-	for (int i = 0; i < sumeet.length(); i++) {
-		sumit *= (int)sumeet[i];
-	}
-	return sumit;
+    long long sumit = 1;
+    for (int i = 0; i < sumeet.length(); i++)
+    {
+        sumit *= (int)sumeet[i];
+    }
+    return sumit;
 }
 int main()
 {
-	string sumeet;
-    cout<<"Enter the word"<<endl;
-    cin>>sumeet;
+    string sumeet;
+    cout << "Enter the word" << endl;
+    cin >> sumeet;
 
-	cout << Ascii(sumeet)<<endl;
-return 0;
+    cout << "The Ascii value is " << Ascii(sumeet) << endl;
+    return 0;
 }
